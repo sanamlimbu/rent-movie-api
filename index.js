daba3676d04f09a3a010a135ab2c467a27af60cf
Sanam Limbu <sanamlimbu08@gmail.com>
2021-04-17 02:22:40 +0800

    Add support for connStr via an env variable.

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ require("./startup/db")();
 //require("./startup/logging")();
 require("./startup/config")();
 require("./startup/validation")();
+require("./startup/prod")(app);
 
 const port = process.env.PORT || 3000;
 //app.listen(port, () => winston.info(`Listening on port ${port}...`));
